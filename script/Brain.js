ec76c9c986b1ca187f78c3b9b7c376fac974174f
Мария <poteryaeva.mariya@yandex.ru>
2022-05-23 22:51:30 +0500

    Нейронные сети по ЛР №5

---
+++ script/Brain.js
@@ -0,0 +1,159 @@
+﻿//Реализация основной функции (ссылки на объекты)
+var Brain = (function () {
+
+    function Brain(brainData) {
+        this.Layers = []; //создание слоя
+
+        if (brainData !== null) {
+            for (var i = 0; i < brainData.Layers.length; i++) {
+                var layerData = brainData.Layers[i];
+                var layer = new Layer();
+                this.Layers.push(layer);
+                for (var j = 0; j < layerData.Neurons.length; j++) {
+                    var neuronData = layerData.Neurons[j];
+                    var neuron = new Neuron();
+                    neuron.AxonValue = neuronData.AxonValue;
+                    neuron.Name = neuronData.Name;
+                    layer.Neurons.push(neuron);
+                    if (i > 0) {
+                        //Соединений нейронов с другими на предыдущем слое
+                        this.Layers[i - 1].ConnectNeuron(neuron);
+                    }
+                    //Установка веса
+                    for (var k = 0; k < neuronData.Dendrites.length; k++) {
+                        neuron.Dendrites[k].Weight = neuronData.Dendrites[k].Weight;
+                    }
+                }
+
+            }
+        }
+    }
+
+    //Для запуска сети вызов метода каждого слоя
+    Brain.prototype.Think = function () {
+        for (var i = 0; i < this.Layers.length; i++) {
+            this.Layers[i].Think();
+        }
+    };
+
+    //Обработка выходного нейрона
+    Brain.prototype.Train = function (inputData, outputNeuron) {
+
+        if (this.Layers.length === 0) {
+            return;
+        }
+
+        //Заполнение первого слоя для подачи в сеть
+        var inputLayer = this.Layers[0];
+        for (var i = 0; i < inputData.length; i++) {
+            inputLayer.Neurons[i].AxonValue = inputData[i];
+        }
+
+        //Генерация выходных данных
+        this.Think();
+
+        //Регуляция значений при помощи дельты
+        //Вычитание представляет собой ошибку, которая и  будет исправлена путем корректировки веса
+        var delta = 0;
+        var learningRate = 0.01;
+        for (var i = 0; i < outputNeuron.Dendrites.length; i++) {
+            var dendrite = outputNeuron.Dendrites[i];
+            delta = parseFloat(Math.max(inputData[i], 0) - outputNeuron.AxonValue);
+            dendrite.Weight += parseFloat(Math.max(inputData[i], 0) * delta * learningRate);
+        }
+    }
+    return Brain;
+})();
+
+//Слой представляет собой набор нейронов
+var Layer = (function () {
+
+    function Layer(neuronCount) {
+        var neuronsToAdd = typeof neuronCount !== "undefined" ? neuronCount : 0;
+        this.Neurons = [];
+
+        //Создание объектов
+        for (var i = 0; i < neuronsToAdd; i++) {
+            this.Neurons.push(new Neuron());
+        }
+    }
+
+    //Все нейроны генерирую выходное значение
+    Layer.prototype.Think = function () {
+        for (var i = 0; i < this.Neurons.length; i++) {
+            this.Neurons[i].Think();
+        }
+    };
+
+    //Соединяет нейрон из другого слоя со всеми нейронами в этом слое
+    Layer.prototype.ConnectNeuron = function (neuron) {
+        for (var i = 0; i < this.Neurons.length; i++) {
+            neuron.Dendrites.push(new Dendrite(this.Neurons[i]))
+        }
+    };
+
+    //Поиск нейрона по имени
+    Layer.prototype.GetNeuron = function (name) {
+        for (var i = 0; i < this.Neurons.length; i++) {
+            if (this.Neurons[i].Name.toUpperCase() === name.toUpperCase()) {
+                return this.Neurons[i];
+            }
+        }
+        return null;
+    };
+
+    //Возвращает нейрон с наибольшим значением аксона в этом слое
+    Layer.prototype.BestGuess = function () {
+        var max = 0;
+        var bestGuessIndex = 0;
+
+        //Нахождение индекса нейрона с наибольшим значением аксона
+        for (var i = 0; i < this.Neurons.length; i++) {
+            if (this.Neurons[i].AxonValue > max) {
+                max = this.Neurons[i].AxonValue;
+                bestGuessIndex = i;
+            }
+        }
+        return this.Neurons[bestGuessIndex];
+    }
+    return Layer;}
+)();
+
+//Нейрон-вычислительной единицей и отвечает за генерацию выходного значения.
+// Реализация функции
+var Neuron = (function () {
+
+    //Функция-конструктор, на входе получает name
+    function Neuron(name) {
+        this.Name = name;
+        this.Dendrites = [];
+        this.AxonValue = 0.5;
+    }
+
+    //Генерация выходного значение (входные значения на соответ. вес)
+    //Выходное значение находится в пределах 0 и 1
+    Neuron.prototype.Think = function () {
+        var sum = 0;
+        if (this.Dendrites.length > 0) {
+            for (var i = 0; i < this.Dendrites.length; i++) {
+                sum += this.Dendrites[i].SourceNeuron.AxonValue * this.Dendrites[i].Weight;
+            }
+
+            //Применение встроенной функции для преобразования суммы
+            this.AxonValue = 1 / (1 + Math.exp(-sum));
+        }
+    };
+    return Neuron;
+}
+)();
+
+//Дендрит представляет собой входное соединение с нейроном.
+//Исходный нейрон, к которому он подключен, должен быть передан через конструктор.
+var Dendrite = (function () {
+    function Dendrite(sourceNeuron) {
+        this.SourceNeuron = sourceNeuron;
+        this.Weight = 0;
+    }
+    return Dendrite;
+}
+)();
---
+++ script/Ocr.js
@@ -0,0 +1,60 @@
+﻿//Создание глобальных переменных
+var brain = new Brain(BRAIN_ABC);
+
+//Обработчик события по нажатию кнопки
+function readDrawing() {
+    //Уменьшение размера рисунка до сетки 15x15, для того, чтобы в переменной 255 элементов с нулями и единицами
+    var drawing = getDownsampledDrawing();
+
+    //inputLayer- это первый слой, outputLayer- последний
+    var inputLayer = brain.Layers[0];
+    var outputLayer = brain.Layers[brain.Layers.length - 1];
+
+    //Заполнение первого входного слоя
+    for (var i = 0; i < drawing.length; i++) {
+        inputLayer.Neurons[i].AxonValue = drawing[i];
+    }
+
+    //Вызов метода
+    brain.Think();
+
+    $("#outputValues").html("<h3>Output values</h3>");
+    for (var i = 0; i < outputLayer.Neurons.length; i++) {
+        var neuron = outputLayer.Neurons[i];
+        $("#outputValues").append("<span>" + neuron.Name.toUpperCase() + ": " + neuron.AxonValue + "</span>");
+    }
+    var bestGuess = outputLayer.BestGuess();
+    if (bestGuess !== null) {
+        $("#bestGuess").text(bestGuess.Name.toUpperCase());
+
+    }
+    else {
+        $("#outputValues").html("Could not read your drawing.");
+    }
+
+}
+
+//Функция запуска
+function trainCharacter() {
+    var inputLayer = brain.Layers[0];
+    var outputLayer = brain.Layers[brain.Layers.length - 1];
+
+    //Определение символа
+    var character = $("#txtCharacter").val().toUpperCase();
+    if (character.length === 0) {
+        return;
+    }
+
+    //Нахождение выходного нейрона
+    var outputNeuron = outputLayer.GetNeuron(character);
+
+    //Если отсутствует значение, то добавление на выходной слой
+    if (outputNeuron === null) {
+        var outputNeuron = new Neuron(character)
+        inputLayer.ConnectNeuron(outputNeuron);
+        outputLayer.Neurons.push(outputNeuron);
+    }
+
+    //Обучение по текущему рисунку
+    brain.Train(getDownsampledDrawing(), outputNeuron);
+}
